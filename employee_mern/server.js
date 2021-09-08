c967fb41eda893fa908e7e71758c31bbaa94b896
henybeza <henny8000@gmail.com>
2021-09-08 01:51:39 -0700

    Added the procfile and the gitignore file inside the root folder

--- employee_mern/server.js
+++ employee_mern/server.js
@@ -40,12 +40,12 @@ app.use('/employees', employeesRouter);
 
 
 //Serving STATIC ASSETS or the build folder if in production...
- //if(process.env.NODE_ENV ==='production'){
+ if(process.env.NODE_ENV ==='production'){
  	//Set static folder
- 	//app.use(express.static('.../build'));
- 	//app.get('*',(req,res) =>{
- 	//	res.sendFile(path.resolve(__dirname, '.../', 'build','index.html'));
- 	//});
+ 	app.use(express.static('client/build'));
+ 	app.get('*',(req,res) =>{
+ 		res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
+ 	});
  //}
 
 
